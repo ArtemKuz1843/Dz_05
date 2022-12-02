8443ac16ff41b601825ba702c379742589ff10af
ArtemKuz <KristofRamuald-1843@yandex.ru>
2022-12-02 23:30:38 +0300

    dz5+

--- dz205ex03_MassSumMaxIMinEl/Program.cs
+++ dz205ex03_MassSumMaxIMinEl/Program.cs
@@ -20,38 +20,29 @@ int[] FillArray(int[] arr)
     var result = new int[arr.Length];
     for(int i = 0; i < arr.Length; i++)
     {
-        result[i] = new Random().Next(1, 10);
+        result[i] = new Random().Next(-100, 101);
     }
     return result;
 }
 
 var newArray = FillArray(array);
 
-PrintArray(newArray); // массив, заполненный случайными числами, 1-10
+PrintArray(newArray); // массив, заполненный случайными числами, от -100 до 100
 
 Console.WriteLine();
 
-int SerchMax(int[] arr)
+int Difference(int[] arr)
 {
     int max = arr[0];
-        for(int i = 0; i < arr.Length; i++)
+    int min = arr[0];
+    for(int i = 0; i < arr.Length; i++)
     {
         if (arr[i] > max) max = arr[i];
-    }
-    return max;
-}
-Console.WriteLine(SerchMax(newArray));
-
-int SerchMin(int[] arr)
-{
-    int min = arr[0];;
-        for(int i = 0; i < arr.Length; i++)
-    {
         if (arr[i] < min) min = arr[i];
     }
-    return min;
+    Console.WriteLine($"Минимальный элемент {min}");
+    Console.WriteLine($"Максимальный элемент {max}");
+    int dif = max - min;
+    return dif;
 }
-Console.WriteLine(SerchMin(newArray));
-
-
-Console.WriteLine($"Разницу между максимальным и минимальным элементом массива: {SerchMax(newArray)-SerchMin(newArray)}");
+Console.WriteLine(Difference(newArray));
