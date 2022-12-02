8453c68801cbb73112f22f55e6a6ea84f29b509f
ArtemKuz <KristofRamuald-1843@yandex.ru>
2022-12-02 19:45:09 +0300

    ДЗ 5

---
+++ dz205ex03_MassSumMaxIMinEl/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+int[] array = new int[5];
+
+void PrintArray(int[] arr)
+{
+    for(int i =0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+}
+
+PrintArray(array); // массив создан и заполнен нулями
+
+Console.WriteLine();
+
+int[] FillArray(int[] arr)
+{
+    var result = new int[arr.Length];
+    for(int i = 0; i < arr.Length; i++)
+    {
+        result[i] = new Random().Next(1, 10);
+    }
+    return result;
+}
+
+var newArray = FillArray(array);
+
+PrintArray(newArray); // массив, заполненный случайными числами, 1-10
+
+Console.WriteLine();
+
+int SerchMax(int[] arr)
+{
+    int max = arr[0];
+        for(int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > max) max = arr[i];
+    }
+    return max;
+}
+Console.WriteLine(SerchMax(newArray));
+
+int SerchMin(int[] arr)
+{
+    int min = arr[0];;
+        for(int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < min) min = arr[i];
+    }
+    return min;
+}
+Console.WriteLine(SerchMin(newArray));
+
+
+Console.WriteLine($"Разницу между максимальным и минимальным элементом массива: {SerchMax(newArray)-SerchMin(newArray)}");
